79eac25bf84a5315a83a22ed4dbbdec4060181a2
Rasul Iskandarov <73297277+rasul-isk@users.noreply.github.com>
2023-05-08 23:26:36 +0300

    removed backend from repo

--- cinemate_front/server/email-service/src/main/java/com/esiproject2023/emailservice/repository/EmailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esiproject2023.emailservice.repository;
-
-import com.esiproject2023.emailservice.model.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmailRepository extends JpaRepository<Email, Long> {
-}
--- cinemate_front/server/metadata-service/src/main/java/com/esiproject2023/metadataservice/model/Metadata.java
+++
@@ -1,5 +0,0 @@
-package com.esiproject2023.metadataservice.model;
-
-
-public record Metadata(String id, String title, double rating, String cast, String description, String genre, String poster, String release_date, String director) {
-}
